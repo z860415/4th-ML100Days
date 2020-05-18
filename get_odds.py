15f54903d96a3051efa1b5bf520b92df5d3656ea
z860415 <z860415@gmail.com>
2020-05-18 19:14:45 +0800

    Update get_odds.py

--- get_odds.py
+++ get_odds.py
@@ -80,6 +80,8 @@ for year in range (2010,2020):
     error_list = []
     error_code = []
     nan_list = []
+    error_list2 = []
+    error_code2 = []
     # pd.valuse.tolist 輸出為[[url]] 多寫迴圈拆開
     for i in odds_list1:
         for j in i:
@@ -107,7 +109,7 @@ for year in range (2010,2020):
             over_odds = data.loc[0, 'Over']
             under_odds = data.loc[0, 'Under']
 
-            if float(over_odds) > 2 or float(over_odds) <= 1.8:
+            if float(over_odds) > 2 or float(over_odds) <= 1.8 and len(over_odds) > 1 :
                 try:
                     data2 = DataFrame(tables[1])
                     over_odds2 = data2.loc[0, 'Over']
@@ -118,7 +120,7 @@ for year in range (2010,2020):
                 except:
                     games_detail.append([date_day, date_time, team_home, team_away, total, over_odds, under_odds])
                     print(date_day, date_time, team_home, team_away, total, over_odds, under_odds)
-            elif over_odds == '' :
+            elif len(over_odds) < 1  :
                 nan_list.append(n)
             else:
                 games_detail.append([date_day, date_time, team_home, team_away, total, over_odds, under_odds])
@@ -135,22 +137,89 @@ for year in range (2010,2020):
         print('第%s筆網頁完成，共耗時%0.2f秒'%((len(games_detail)+len(error_list)),end1-start1))
 
 
-
-    data = pd.DataFrame(games_detail,columns=column_list)
-    data.to_csv(f"Season_%s_data.csv"%year, encoding='utf_8_sig')
-
-    error_csv = pd.DataFrame(error_list)
-    error_csv.to_csv(f"Season_%s_error_list.csv" % year, encoding='utf_8_sig')
-
-    error_code_csv = pd.DataFrame(error_code)
-    error_code_csv.to_csv(f"Season_%s_error_code.csv" % year, encoding='utf_8_sig')
-
-    nan_csv = pd.DataFrame(nan_list)
-    nan_csv.to_csv(f"Season_%s_nan_list.csv" % year, encoding='utf_8_sig')
-
-
-
     end0=time.time()
     print('%s賽季爬蟲完成，共%s筆網頁，耗時%0.2f秒' % (year,(len(games_detail) + len(error_list)), end0 - start0))
     print('共' + str(len(error_list)) + '筆網頁異常')
     print('共' + str(len(nan_list)) + '筆網頁賠率空值')
+    print('error_list網頁爬蟲開始執行')
+
+    start3 = time.time()
+    for error_url in error_list:
+        start4 = time.time()
+        try:
+            driver = webdriver.Chrome("./chromedriver.exe", options=options)
+            driver.implicitly_wait(30)
+            html = driver.get('error_url')
+            # sleep(5)
+            html_data = driver.page_source
+            driver.close()
+            soup = BeautifulSoup(html_data, 'html.parser')
+            total = soup.select('div[id="odds-data-table"] a ')[0].text.split('Under ')[1]
+            under_odds = soup.select('span[class="avg chunk-odd nowrp"] a ')[0].text
+            over_odds = soup.select('span[class="avg chunk-odd nowrp"] a ')[1].text
+            team = soup.select('div[id="col-content"] h1')[0].text
+            date_all = soup.select('div[id="col-content"] p')[0].text
+            team_home = team.split(' - ')[0]
+            team_away = team.split(' - ')[1]
+            date_day = date_all.split(', ')[1]
+            date_time = date_all.split(', ')[2]
+
+            if float(over_odds) > 2 or float(over_odds) <= 1.8 and len(over_odds) > 1:
+                try:
+                    over_odds2 = soup.select('span[class="avg chunk-odd nowrp"] a ')[3].text
+                    under_odds2 = soup.select('span[class="avg chunk-odd nowrp"] a ')[2].text
+                    total2 = soup.select('div[id="odds-data-table"] a ')[4].text.split('Under ')[1]
+
+                    if float(over_odds2) > 2 or float(over_odds2) <= 1.8 and len(over_odds2) > 1:
+                        try:
+                            over_odds3 = soup.select('span[class="avg chunk-odd nowrp"] a ')[5].text
+                            under_odds3 = soup.select('span[class="avg chunk-odd nowrp"] a ')[4].text
+                            total3 = soup.select('div[id="odds-data-table"] a ')[8].text.split('Under ')[1]
+                            games_detail.append(
+                                [date_day, date_time, team_home, team_away, total3, over_odds3, under_odds3])
+                            print([date_day, date_time, team_home, team_away, total3, over_odds3, under_odds3])
+                        except:
+                            games_detail.append(
+                                [date_day, date_time, team_home, team_away, total2, over_odds2, under_odds2])
+                            print([date_day, date_time, team_home, team_away, total2, over_odds2, under_odds2])
+                    elif len(over_odds) < 1:
+                        nan_list.append(error_url)
+                    else:
+                        games_detail.append(
+                            [date_day, date_time, team_home, team_away, total2, over_odds2, under_odds2])
+                        print([date_day, date_time, team_home, team_away, total2, over_odds2, under_odds2])
+                except:
+                    games_detail.append([date_day, date_time, team_home, team_away, total, over_odds, under_odds])
+                    print(date_day, date_time, team_home, team_away, total, over_odds, under_odds)
+
+            elif len(over_odds) < 1:
+                nan_list.append(error_url)
+            else:
+                games_detail.append([date_day, date_time, team_home, team_away, total, over_odds, under_odds])
+                print(date_day, date_time, team_home, team_away, total, over_odds, under_odds)
+
+        except Exception as e:
+            print(e)
+            print(error_url + '網頁異常請確認')
+            error_list2.append(error_url)
+            error_code2.append(e)
+            continue
+        end4 = time.time()
+        print('網頁爬蟲完成，共耗時%0.2f秒' % (end4 - start4))
+
+    end3 = time.time()
+    print('%s賽季爬蟲完成，共%s筆網頁，耗時%0.2f秒' % (year, (len(games_detail) + len(error_list) + len(error_list2)), end3 - start3))
+    print('共' + str(len(error_list2)) + '筆網頁異常')
+    print('共' + str(len(nan_list)) + '筆網頁賠率空值')
+
+    data = pd.DataFrame(games_detail,columns=column_list)
+    data.to_csv(f"Season_%s_data.csv"%year, encoding='utf_8_sig')
+
+    error_csv = pd.DataFrame(error_list2)
+    error_csv.to_csv(f"Season_%s_error_list.csv" % year, encoding='utf_8_sig')
+
+    error_code_csv = pd.DataFrame(error_code2)
+    error_code_csv.to_csv(f"Season_%s_error_code.csv" % year, encoding='utf_8_sig')
+
+    nan_csv = pd.DataFrame(nan_list)
+    nan_csv.to_csv(f"Season_%s_nan_list.csv" % year, encoding='utf_8_sig')
